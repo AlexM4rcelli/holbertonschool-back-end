679f2a85e196e6998334e1064d0571b283eeefe8
AlexM4rcelli <6780@holbertonstudents.com>
2023-12-21 20:57:35 -0300

    Feat: Task 0 and 1

---
+++ 0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+users = requests.get(f'https://jsonplaceholder.typicode.com/users')
+
+if users.status_code == 200:
+    users_data = users.json()
+    for usr in users_data:
+        if usr['id'] == int(sys.argv[1]):
+            user = usr
+
+user_todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{user['id']}/todos')
+
+if user_todos.status_code == 200:
+    todos_data = user_todos.json()
+    tasks_done = [task for task in todos_data if task['completed']]
+    print(f"Employee {user['name']} is done with tasks({len(tasks_done)}/{len(todos_data)})")
+    for task in tasks_done:
+        print('\t ' + task['title'])
---
+++ 1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests, sys , csv
+
+users = requests.get(f'https://jsonplaceholder.typicode.com/users')
+
+if users.status_code == 200:
+    users_data = users.json()
+    for usr in users_data:
+        if usr['id'] == int(sys.argv[1]):
+            user = usr
+
+user_todos = requests.get(f'https://jsonplaceholder.typicode.com/users/{user['id']}/todos')
+
+if user_todos.status_code == 200:
+    todos_data = user_todos.json()
+    tasks_done = [task for task in todos_data if task['completed']]
+    with open(f'{sys.argv[1]}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todos_data:
+            writer.writerow([str(user['id']), user['username'], str(task['completed']), task['title']])
