5b217259afe11aacc98118726cb4f0bb8c069b27
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-14 00:26:50 +0000

    Fix: Some errors

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     collection = client.logs.nginx
-
+    print(f'{collection.count_documents({})} logs')
     for mt in methods:
         print(f'\tmethod {mt}: {collection.count_documents({"method": mt})}')
     print(f'{collection.count_documents({"method": "GET", "path": "/status"})}'
