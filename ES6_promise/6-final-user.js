977e24f8cec0b32b5762f06aa820a298675ecf30
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-28 10:53:26 -0300

    Feat: All tasks

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import { uploadPhoto } from './utils';
+import signUpUser from './4-user-promise';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = uploadPhoto();
+  const promise2 = signUpUser(firstName, lastName, fileName);
+
+  return Promise.all([promise1, promise2])
+    .then((results) => results.map((result) => ({ status: 'fulfilled', value: result })))
+    .catch((error) => ({ status: 'rejected', value: error }));
+}
