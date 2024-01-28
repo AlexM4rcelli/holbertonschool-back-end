417c11c95503bc486067d16254c011c0f1ff8864
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-28 12:05:15 -0300

    Use allSettled to get all promises resolved and rejected

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,10 +2,12 @@ import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = uploadPhoto(fileName);
-  const promise2 = signUpUser(firstName, lastName, fileName);
+  const promise1 = signUpUser(firstName, lastName, fileName);
+  const promise2 = uploadPhoto(fileName);
 
-  return Promise.all([promise1, promise2])
-    .then((results) => results.map((result) => ({ status: 'fulfilled', value: result })))
-    .catch((error) => ({ status: 'rejected', value: error.toString() }));
+  return Promise.allSettled([promise1, promise2])
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.value || res.reason.toString(),
+    })));
 }
