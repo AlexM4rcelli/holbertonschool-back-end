7cc6759408744d499ed224c7eafcf4d307d6cea8
AlexM4rcelli <6780@holbertonstudents.com>
2024-01-28 11:17:16 -0300

    Fix: Import statement

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
-import { uploadPhoto } from './utils';
+import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = uploadPhoto();
+  const promise1 = uploadPhoto(fileName);
   const promise2 = signUpUser(firstName, lastName, fileName);
 
   return Promise.all([promise1, promise2])
